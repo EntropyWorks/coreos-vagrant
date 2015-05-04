bc24db868430d4908f78a1bb98639200a40c562e
Yazz Atlas <yazz.atlas@hp.com>
2015-05-04 01:19:53 -0700

    Now making parts so I can talk about them

--- record-it.sh
+++ record-it.sh
@@ -5,5 +5,15 @@
 #   asciinema auth
 #   asciinema -h | --help
 #   asciinema --version
-clear
-asciinema rec -t do-it.sh -c "./do-it.sh -p vmware_fusion" -w 1 -y /tmp/do-it.record
+function record(){
+    clear
+    command=$1
+    title=$2
+    filename=${title// /_}.record
+    asciinema rec -t "${title}" -c "${command}" -w 1 -y asciinema/${filename}
+}
+
+record ./step-01.sh "Step 01 - Deploy CoreOS"
+record ./step-02.sh "Step 02 - Deploy Fleet Units"
+record ./step-03.sh "Step 03 - Start Fleet Units"
+
